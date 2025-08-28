465f9d4cfff760fe2646be40c2ef039daa61c994
angel <angelledesma180@gmail.com>
2025-08-28 08:53:38 -0500

    Add Assignment 2

---
+++ Week2/Assignment/assignment2/assignment2.js
@@ -0,0 +1,74 @@
+// Challenge 1
+const name = "Angel";
+let age = "24";
+let enrollment = true;
+
+console.log("Challenge 1");
+console.log(name);
+console.log(age);
+console.log(enrollment);
+console.log("-----------");
+// Challenge 2
+console.log("Challenge 2");
+let a = 24;
+let b = 43;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log("-----------");
+// Challenge 3
+console.log("Challenge 3");
+let str = "This is a string containing a sentence";
+console.log(str.length);
+console.log(str[0]);
+console.log(str[str.length - 1]);
+console.log("-----------");
+
+// Challenge 4
+console.log("Challenge 4");
+let num = -43;
+console.log(Math.sqrt(num));
+console.log(Math.pow(num, 2));
+console.log(Math.abs(num));
+console.log("-----------");
+
+// Challenge 5
+console.log("Challenge 5");
+let x = 20;
+let y = -32;
+console.log("20 > -32");
+console.log(x > y);
+console.log("20 < -32");
+console.log(x < y);
+console.log("20 = -32");
+console.log(x === y);
+console.log("-----------");
+
+// Challenge 6
+console.log("Challenge 6");
+
+let bool1 = true;
+let bool2 = true;
+
+console.log("AND");
+console.log(bool1 && bool2);
+console.log("OR");
+console.log(bool1 || bool2);
+console.log("NOT");
+console.log(!bool1 && bool2);
+console.log(!bool1 || bool2);
+
+console.log("-----------");
+
+// Challenge 7
+console.log("Challenge 7");
+let firstName = "Angel";
+let lastName = "Ledesma";
+let greetingMessage = `Hi my name is ${
+  firstName + " " + lastName
+} nice to meet you`;
+console.log(greetingMessage);
+
+console.log("-----------");
